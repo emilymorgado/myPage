edbe53692aabad2d8d674e1e0ab37d24690668c5
Emily Morgado <emilyannemorgado@gmail.com>
2015-12-05 23:06:21 -0800

    built a sql/database page

--- server.py
+++ server.py
@@ -46,7 +46,7 @@ def sql():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
-    app.debug = False
+    app.debug = True
 
     # connect_to_db(app)
 
